8e0351ff5d997e1d3fffb0686e5f7be674fc83a7
nhattinh2601 <nhattinh2601@gmail.com>
2023-07-28 17:16:05 +0700

    up

---
+++ WebAPI/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public CartController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public List<CartItem> Carts
+        {
+            get
+            {
+                var data = HttpContext.Session.Get<List<CartItem>>("GioHang");
+                if (data == null)
+                {
+                    data = new List<CartItem>();
+                }
+                return data;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(Carts);
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(Guid id, int SoLuong)
+        {
+            var myCart = Carts;
+            var item = myCart.SingleOrDefault(p => p.product_id == id);
+
+            if (item == null)//chưa có
+            {
+                var hangHoa = _context.Products.SingleOrDefault(p => p.id == id);
+                item = new CartItem
+                {
+                    product_id = id,
+                    product_name = hangHoa.name,
+                    price = hangHoa.price,
+                    amount = SoLuong,                    
+                };
+                myCart.Add(item);
+            }
+            else
+            {
+                item.amount += SoLuong;
+            }
+            HttpContext.Session.Set("GioHang", myCart);
+
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Models
+{
+    public class CartItem
+    {
+        public Guid product_id { get; set; }
+        public string product_name { get; set; }        
+        public double price { get; set; }
+        public int amount { get; set; }
+        public double total_cost => amount * price;
+    }
+}
